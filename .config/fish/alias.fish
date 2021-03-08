986f2a07b7ca41277b4b9306c2f6315eff4fa1d5
seike <seike@posteo.de>
2021-03-08 19:46:57 +0100

    TLC

---
+++ .config/fish/alias.fish
@@ -0,0 +1,9 @@
+alias config='/usr/bin/git --git-dir=$HOME/Repos/dotfiles/ --work-tree=$HOME'
+alias lll='lsd -la'
+
+alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+alias l.='exa -a | egrep "^\."'
+
---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+starship init fish | source
+
+
+source ~/.config/fish/alias.fish                  # Source custom alias
+
+set fish_greeting                                 # Supresses fish's intro message
+set TERM "xterm-256color"                         # Sets the terminal type
+
+
