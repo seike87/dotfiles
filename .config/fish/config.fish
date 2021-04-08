9f89f3c8343387aae918f7285b017187c52f1e71
seike <seike@posteo.de>
2021-04-08 20:41:08 +0200

    More rice

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,7 @@ source ~/.config/fish/alias.fish                  # Source custom alias
 
 set fish_greeting                                 # Supresses fish's intro message
 set TERM "xterm-256color"                         # Sets the terminal type
+set -x MOZ_ENABLE_WAYLAND 1
+# set -x MOZ_DBUS_REMOTE 1
 
 
